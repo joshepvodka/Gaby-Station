f1f5f0c104b911e7de96f6593425fefb3a4bb12b
White <68350815+DoutorWhite@users.noreply.github.com>
2024-08-25 15:58:02 -0300

    Update Content.Server/Communications/CommunicationsConsoleSystem.cs
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>

--- Content.Server/Communications/CommunicationsConsoleSystem.cs
+++ Content.Server/Communications/CommunicationsConsoleSystem.cs
@@ -323,6 +323,7 @@ namespace Content.Server.Communications
         {
             if (!EntityManager.TryGetComponent(message.Actor, out ActorComponent? actor))
                 return;
+
             var mob = message.Actor;
             if (!CanUse(mob, uid))
             {
