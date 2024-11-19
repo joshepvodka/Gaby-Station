475b80aba42c3249d64ff3ce486b1386ff07b3a9
joshepvodka <guilherme.ornel@gmail.com>
2024-11-18 22:29:47 -0300

    adiciona os script

---
+++ Tools/ss14_ru/translation.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+
+call pip install -r requirements.txt --no-warn-script-location
+call py ./yamlextractor.py
+call py ./keyfinder.py
+call py ./clean_duplicates.py
+call py ./clean_empty.py
+
+PAUSE
