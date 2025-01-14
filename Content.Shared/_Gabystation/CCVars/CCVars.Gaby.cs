8940f49daf5fd5d314d52fc2eeb6f9c94db73ee8
joshepvodka <guilherme.ornel@gmail.com>
2025-01-14 18:11:31 -0300

    arruma cvars pt1

---
+++ Content.Shared/_Gabystation/CCVars/CCVars.Gaby.cs
@@ -0,0 +1,13 @@
+using Robust.Shared.Configuration;
+
+namespace Content.Shared._Gabystation.CCVar;
+
+[CVarDefs]
+public sealed partial class GabyCVars
+{
+        /// <summary>
+        /// Enables alternate job titles for players.
+        /// </summary>
+        public static readonly CVarDef<bool> ICAlternateJobTitlesEnable =
+            CVarDef.Create("ic.alternate_job_titles_enable", true, CVar.SERVER | CVar.REPLICATED);
+}
