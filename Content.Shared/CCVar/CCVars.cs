f261f4a54eae7290ba01de4b78361f637edc12d9
joshepvodka <guilherme.ornel@gmail.com>
2025-01-14 18:17:35 -0300

    arruma cvars parte 3

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -52,6 +52,8 @@ public sealed partial class CCVars : CVars
         CVarDef.Create("light_cycle.update_per_tick", 3, desc: "(integer)", flag: CVar.SERVERONLY);
 
     #endregion
+    public static readonly CVarDef<bool> MOTDBuletinEnable =
+       CVarDef.Create("chat.motd_buletin_enable", true, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE, "If the MOTD buletin appears in lobby");
 
     #region SoftCrit
 
