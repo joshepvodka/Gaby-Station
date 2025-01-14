c0030df44178529824ee15841dcf8bf5a3cc4a0a
joshepvodka <guilherme.ornel@gmail.com>
2025-01-14 18:16:38 -0300

    arruma ccvars pt2 (final eu espero)

--- Content.Shared/_Gabystation/CCVars/CCVars.Gaby.cs
+++ Content.Shared/_Gabystation/CCVars/CCVars.Gaby.cs
@@ -10,4 +10,10 @@ public sealed partial class GabyCVars
         /// </summary>
         public static readonly CVarDef<bool> ICAlternateJobTitlesEnable =
             CVarDef.Create("ic.alternate_job_titles_enable", true, CVar.SERVER | CVar.REPLICATED);
+
+        /// <summary>
+        /// If the message of the day lobby widget should be displayed.
+        /// </summary>
+        public static readonly CVarDef<bool> MOTDBuletinEnable =
+            CVarDef.Create("chat.motd_buletin_enable", true, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE, "If the MOTD buletin appears in lobby");
 }
